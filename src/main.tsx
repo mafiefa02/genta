dbcdddf5cf94ebd64753e77cdcf46d929f8de3d5
mafiefa02 <91366510+mafiefa02@users.noreply.github.com>
2026-04-04 12:22:29 +0700

    chore: added temporary test component for the updater

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { UpdateChecker } from "-/components/update-checker";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <p className="font-bold">Hello!</p>
+    <UpdateChecker />
   </React.StrictMode>,
 );
