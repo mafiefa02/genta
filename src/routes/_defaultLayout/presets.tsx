e748f9732b6228d5878b0566b82f459640fd255a
mafiefa02 <91366510+mafiefa02@users.noreply.github.com>
2026-04-09 17:19:03 +0700

    feat: integrated `/presets` query

--- src/routes/_defaultLayout/presets.tsx
+++ src/routes/_defaultLayout/presets.tsx
@@ -1,9 +1,33 @@
+import { configQueries } from "-/hooks/queries/config";
+import { presetsQueries } from "-/hooks/queries/presets";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_defaultLayout/presets")({
   component: RouteComponent,
+  loader: ({ context }) => {
+    return Promise.all([
+      context.queryClient.ensureQueryData(presetsQueries.list()),
+      context.queryClient.ensureQueryData(configQueries.activePresetId()),
+    ]);
+  },
 });
 
 function RouteComponent() {
-  return <div>Hello "/profiles"!</div>;
+  const { data: presets } = useSuspenseQuery(presetsQueries.list());
+  const { data: activePresetId } = useSuspenseQuery(configQueries.activePresetId());
+
+  return (
+    <div>
+      <h1>Presets</h1>
+      <p>Active preset ID: {activePresetId}</p>
+      <ul>
+        {presets.map((preset) => (
+          <li key={preset.id}>
+            {preset.name} {preset.id === activePresetId && "(active)"}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
