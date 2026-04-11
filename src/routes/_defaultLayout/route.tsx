35da39a0546c2d866a530786f1ea9df27dfcec54
mafiefa02 <91366510+mafiefa02@users.noreply.github.com>
2026-04-11 15:24:44 +0700

    style: added subtle gradient on main container

--- src/routes/_defaultLayout/route.tsx
+++ src/routes/_defaultLayout/route.tsx
@@ -14,7 +14,7 @@ function RouteComponent() {
         <AppSidebar />
         <div className="flex min-w-0 flex-1 flex-col">
           <AppHeader />
-          <main className="content flex-1 overflow-y-auto px-4 py-3">
+          <main className="content flex-1 overflow-y-auto bg-linear-to-b from-background via-background to-primary/5 px-4 py-3">
             <Outlet />
           </main>
         </div>
